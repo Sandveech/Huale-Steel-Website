0bf1f22e450f116f90d0cd19401fb05a7c4277bf
Sandveech <82623509+Sandveech@users.noreply.github.com>
2025-09-10 22:13:55 +0700

    Hompage revamp

---
+++ js/components/nav-menu.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navList = document.querySelector(".nav-list");
+
+navToggle.addEventListener("click", () => {
+    navToggle.classList.toggle("active");
+    navList.classList.toggle("show");
+})
